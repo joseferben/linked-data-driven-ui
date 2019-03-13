b7668ba838cbe3b8eb5bd8ace3201a3e3a7853ca
Josef Erben <josef@erben.io>
2019-03-13 21:18:13 +0100

    Switch jsonld wrappers to use promise based API
    Turning promises into chanels is simpler and more
    elegant than using callbacks.

--- ui/src/main/ui/frontend/core/jsonld.cljs
+++ ui/src/main/ui/frontend/core/jsonld.cljs
@@ -1,24 +1,19 @@
 (ns ui.frontend.core.jsonld
   (:require ["jsonld" :as jsonld]
             [clojure.core.async :refer-macros [go]]
-            [clojure.core.async :as a]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.core.async :as a]))
 
 (defn compact [doc ctx]
   (let [c (a/chan)]
-    (.compact jsonld (clj->js doc) (clj->js ctx)
-              (fn [err compacted]
-                (when err
-                  (js/console.error err))
-                (a/put! c (js->clj compacted))))
+    (-> (.compact jsonld (clj->js doc) (clj->js ctx))
+        (.then (fn [compacted] (a/put! c (js->clj compacted)))
+               (fn [err] (js/console.error err))))
+
     c))
 
 (defn expand [compacted]
   (let [c (a/chan)]
-    (.expand jsonld (clj->js compacted)
-             (fn [err expanded]
-               (when err
-                 (js/console.error err))
-               (pprint expanded)
-               (a/put! c (js->clj expanded))))
+    (-> (.expand jsonld (clj->js compacted))
+        (.then (fn [expanded] (a/put! c (js->clj expanded)))
+               (fn [err] (js/console.error err))))
     c))
